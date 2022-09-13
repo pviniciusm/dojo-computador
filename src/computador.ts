fca0a64dd8639c535e2f88743f3ad4e0f8885a3f
Jean Terhorst <acebola08@gmail.com>
2022-09-13 20:06:41 -0300

    classe computador

---
+++ src/computador.ts
@@ -0,0 +1,26 @@
+/* 1) Crie uma classe chamada Computador
+
+2) A classe deve conter as seguintes PROPRIEDADES
+    - processador (string)
+    - ram (número)
+    - capacidadeHd (numero)
+    - indLigado (boolean)
+    - games (lista de Game) (opcional)
+
+3) Crie o construtor da classe com os atributos obrigatórios como parâmetro
+
+4) Mantenha os atributos encapsulados
+
+5) A classe deve conter os seguintes MÉTODOS
+    - ligarComputador (void), muda o indLigado para true
+		- avisar */
+
+    import {Game} from "./game"
+
+        class Computador{
+            private _processador: string;
+            private _ram: number;
+            private _capacidadeHd: number;
+            private _indLigado: boolean;
+            private _games?:<>;
+        }
--- src/game.ts
+++ src/game.ts
@@ -37,3 +37,4 @@ class Game {
     return this._minRam;
   }
 }
+export {Game};
