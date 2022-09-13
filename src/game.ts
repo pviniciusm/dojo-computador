66d8ec97309450b7b1071f045166efc93b8e518f
Lucas Silveira <lucasroennausilveira@gmail.com>
2022-09-13 19:51:00 -0300

    atualizaçao

--- src/game.ts
+++ src/game.ts
@@ -12,15 +12,28 @@ class Game {
   private _nome: String;
   private _tamanho: number;
   private _genero: String;
-  private _minRam: number;
+  private _minRam?: number;
 
-  constructor(nome: String, tamanho: number, minRam: number) {
+  constructor(nome: String, tamanho: number, genero: String, minRam?: number) {
     this._nome = nome;
     this._tamanho = tamanho;
+    this._genero = genero;
     this._minRam = minRam;
   }
 
-  public getNome() {
+  public get nome() {
     return this._nome;
+  }
+
+  public get tamanho() {
+    return this._tamanho;
+  }
+
+  public get genero() {
+    return this._genero;
+  }
+
+  public get minRam() {
+    return this._minRam;
   }
 }
