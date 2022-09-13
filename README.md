ccd0774aae182e1d9febe6bab47170454f7770b6
Paulo Vinicius Cardoso <pvinics@gmail.com>
2022-09-13 19:08:59 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Template-7ed-starter
+Template para as aulas da 7a edição do programa Starter Web Full Stack - Growdev
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+/**
+ * -----------------------------------------
+ * ---         Growdev 7a Edição         ---
+ * ---    Template p/ projetos Node.js   ---
+ * ---  Programa Starter Web Full Stack  ---
+ * -----------------------------------------
+ */
+
+console.log("It works!");
